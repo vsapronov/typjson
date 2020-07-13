7aa9bd1b0747285f621e7df533ce2cb9258d6a4b
Vladimir Sapronov <v.sapronov@vladsmac.fios-router.home>
2020-07-13 04:53:13 -0400

    Fixed tests names

--- test_json.py
+++ test_json.py
@@ -18,79 +18,75 @@ def check_type_error(typ, data, json_str):
         loads(typ, json_str)
 
 
-def test_int_serialization():
+def test_int():
     check_success(int, 3, '3')
 
 
-def test_int_serialization_boolean():
+def test_int_boolean():
     check_type_error(int, True, 'true')
 
 
-def test_int_deserialization_wrong_type():
+def test_int_wrong_type():
     check_type_error(int, '3', '"3"')
 
 
-def test_float_serialization_fractional():
+def test_float():
     check_success(float, 1.23, '1.23')
 
 
-def test_float_deserialization_whole():
+def test_float_int_json():
     assert float(1) == loads(float, '1')
 
 
-# def test_decimal_serialization():
-#     check_serialization(Decimal, Decimal(1.23), '1.23')
-
-
-def test_str_serialization():
+def test_str():
     check_success(str, 'bla', '"bla"')
 
 
-def test_serialization_null_safety():
+def test_null_safety():
     check_type_error(str, None, 'null')
 
 
-def test_bool_serialization():
+def test_bool():
     check_success(bool, True, 'true')
 
 
-def test_none_serialization():
+def test_none():
     check_success(NoneType, None, 'null')
 
 
-def test_date_serialization():
+def test_date():
     check_success(date, date(year=2020, month=1, day=1), '"2020-01-01"')
 
 
-def test_none_serialization_wrong_type():
+def test_none_wrong_type():
     check_type_error(NoneType, 'bla', '"bla"')
 
 
-def test_list_serialization():
+def test_generic_list():
     check_success(List[int], [2, 3], '[2, 3]')
 
 
-def test_list_of_date_serialization():
+def test_generic_list_of_date():
     check_success(List[date], [date(year=2020, month=1, day=2)], '["2020-01-02"]')
 
 
-def test_dict_serialization():
+def test_generic_dict():
     check_success(Dict[str, date], {'key': date(year=2020, month=1, day=2)}, '{"key": "2020-01-02"}')
 
 
-def test_dict_serialization_wrong_key_type():
+def test_generic_dict_wrong_key_type():
     check_type_error(Dict[int, date], {2: date(year=2020, month=1, day=2)}, '{"2": "2020-01-02"}')
 
 
-def test_optional_serialization_none():
+def test_optional_none():
     check_success(Optional[int], None, 'null')
 
 
-def test_optional_serialization_some():
+def test_optional_some():
     check_success(Optional[int], 123, '123')
 
 
-def test_union_primitive_serialization():
+def test_union_primitives():
     check_success(Union[str, int], 'bla', '"bla"')
     check_success(Union[str, int], 3, '3')
 
@@ -105,11 +101,11 @@ class TheClass:
     int_field: int
 
 
-def test_class_serialization():
+def test_dataclass():
     check_success(TheClass, TheClass('bla', 123), '{"string_field": "bla", "int_field": 123}')
 
 
-def test_class_serialization_wrong_type():
+def test_dataclass_wrong_field_type():
     check_type_error(TheClass, TheClass('bla', 'wrong'), '{"string_field": "bla", "int_field": "wrong"}')
 
 
