ee34eb081d184900e9242e77236c8d22bf42a885
Vladimir Sapronov <v.sapronov@vladsmac.fios-router.home>
2020-08-16 16:01:15 -0400

    Implementation of tagged union

---
+++ tests/test_tagged_union.py
@@ -0,0 +1,119 @@
+from typ.tagged_union import *
+
+
+@tagged_union
+class A:
+    Number: int
+    Text: str
+    Unknown: None
+
+
+@tagged_union
+class B:
+    Number: int
+    Text: str
+    Unknown: None
+
+
+def test_is_tagged_union():
+    assert isunion(A)
+    assert not isunion(int)
+
+
+def test_members():
+    assert members(A) == {'Number': int, 'Text': str, 'Unknown': None}
+
+
+def test_members_equality():
+    assert A.Number(2) == A.Number(2)
+    assert A.Number(2) != A.Number(3)
+    assert A.Unknown() == A.Unknown()
+    assert A.Unknown(None) == A.Unknown()
+
+
+def test_creators_equality():
+    assert A.Number == A.Number
+    assert A.Number != B.Number
+    assert A.Text != A.Number
+
+
+def test_isinstance():
+    assert isinstance(A.Text('something'), A)
+    assert isinstance(A.Number(3), A)
+    assert isinstance(A.Unknown(), A)
+
+
+def test_ismember():
+    assert ismember(A.Text('something'), A.Text)
+    assert not ismember(A.Text('something'), A.Number)
+    assert ismember(A.Unknown(), A.Unknown)
+
+
+def test_member_name():
+    assert member_name(A.Text('something')) == "Text"
+    assert member_name(A.Number(3)) == "Number"
+    assert member_name(A.Unknown()) == "Unknown"
+
+
+def test_member_type():
+    assert member_type(A.Text('something')) == str
+    assert member_type(A.Number(3)) == int
+    assert member_type(A.Unknown()) is None
+
+
+def test_create_member():
+    assert create_member(A, "Number", 3) == A.Number(3)
+    assert create_member(A, "Unknown", None) == A.Unknown()
+
+
+def test_member_value():
+    a = A.Text('something')
+    assert 'something' == a.value
+    unknown = A.Unknown()
+    assert unknown.value is None
+
+
+def test_repr():
+    assert repr(A.Text) == "A.Text of <class 'str'>"
+    assert repr(A.Text('something')) == "A.Text('something')"
+    assert repr(A.Unknown()) == "A.Unknown()"
+
+
+def test_match_union():
+    a = A.Text("something")
+    result = match(a, {
+        A.Text: lambda text: f'found: {text}',
+    })
+    assert result == "found: something"
+
+
+def test_match_union_by_value():
+    a = A.Text("something")
+    result = match(a, {
+        A.Text("something"): lambda text: f'something',
+        A.Text: lambda text: f'not something'
+    })
+    assert result == "something"
+    a = A.Text("not something")
+    result = match(a, {
+        A.Text("something"): lambda text: f'something',
+        A.Text: lambda text: f'not something'
+    })
+    assert result == "not something"
+
+
+def test_match_union_default():
+    a = A.Number(3)
+    result = match(a, {
+        A.Text: lambda text: f'found: {text}',
+        default: lambda: 'not found',
+    })
+    assert result == "not found"
+
+
+def test_match_union_no_param():
+    b = B.Unknown()
+    result = b.match({
+        B.Unknown: lambda _: 'unknown',
+    })
+    assert result == "unknown"
---
+++ typ/tagged_union.py
@@ -0,0 +1,137 @@
+__UNION_MEMBERS__ = "__union_members"
+
+
+def tagged_union(union_class):
+    class UnionMember(union_class):
+        def __init__(self, member_name, member_type, value):
+            self._member_name = member_name
+            self._member_type = member_type
+            self.value = value
+            if member_type is not None:
+                self._args = (value,)
+            else:
+                self._args = ()
+
+        def __eq__(self, other):
+            if not isinstance(other, self.__class__):
+                return False
+            return (self._member_name, self.value) == (other._member_name, other.value)
+
+        def __repr__(self):
+            if '__repr__' in vars(union_class):
+                rep = union_class.__repr__(self)
+            else:
+                if self._member_type is None:
+                    rep = f'{union_class.__name__}.{self._member_name}()'
+                else:
+                    rep = f'{union_class.__name__}.{self._member_name}({repr(self.value)})'
+            return rep
+
+        def __hash__(self):
+            return hash((self._member_name, self.value))
+
+        def match(self, cases):
+            return match(self, cases)
+
+    class UnionMemberCreator:
+        def __init__(self, union_type, member_name, member_type):
+            self._union_type = union_type
+            self._member_name = member_name
+            self._member_type = member_type
+
+        def __call__(self, *args):
+            if self._member_type is None:
+                assert len(args) == 0 or len(args) == 1
+                if len(args) == 1:
+                    assert args[0] is None
+                return UnionMember(self._member_name, self._member_type, None)
+            else:
+                assert len(args) == 1
+                arg = args[0]
+                assert isinstance(arg, self._member_type)
+                return UnionMember(self._member_name, self._member_type, arg)
+
+        def __eq__(self, other):
+            if not isinstance(other, UnionMemberCreator):
+                return False
+            return other._union_type == self._union_type and other._member_name == self._member_name
+
+        def __repr__(self):
+            return f'{self._union_type.__name__}.{self._member_name} of {repr(self._member_type)}'
+
+        def __hash__(self):
+            return hash(self._member_name)
+
+    union_members = union_class.__dict__.get('__annotations__', {})
+
+    for name, typ in union_members.items():
+        setattr(union_class, name, UnionMemberCreator(union_class, name, typ))
+
+    setattr(union_class, __UNION_MEMBERS__, union_members)
+
+    return union_class
+
+
+def create_member(typ, member_name, arg=None):
+    if members(typ)[member_name] is None:
+        return getattr(typ, member_name)()
+    else:
+        return getattr(typ, member_name)(arg)
+
+
+def isunion(klass):
+    return hasattr(klass, __UNION_MEMBERS__)
+
+
+def members(klass):
+    if not hasattr(klass, __UNION_MEMBERS__):
+        raise TypeError(f'{klass} is not tagged union')
+    return getattr(klass, __UNION_MEMBERS__)
+
+
+def _isinstance_member_creator(member):
+    return member.__class__.__name__ == "UnionMemberCreator"
+
+
+def _isinstance_member(member):
+    return member.__class__.__name__ == "UnionMember"
+
+
+def ismember(value, member_creator):
+    if not _isinstance_member_creator(member_creator):
+        raise ValueError(f'{member_creator} is not member creator')
+    return isinstance(value, member_creator._union_type) and value._member_name == member_creator._member_name
+
+
+def member_name(obj):
+    if not _isinstance_member(obj):
+        raise ValueError(f'{obj} is not tagged_union member')
+    return obj._member_name
+
+
+def member_type(obj):
+    if not _isinstance_member(obj):
+        raise ValueError(f'{obj} is not tagged_union member')
+    return obj._member_type
+
+
+default = object()
+
+
+def match_case(case_key, obj):
+    if default == case_key:
+        return True
+    if _isinstance_member_creator(case_key):
+        return ismember(obj, case_key)
+    else:
+        return case_key == obj
+
+
+def match(obj, cases):
+    case_key, case_lambda = next(((case_key, case_lambda) for case_key, case_lambda in cases.items() if match_case(case_key, obj)), (None, None))
+    if case_lambda is None:
+        raise ValueError(f'{obj} was not matched by any case')
+    if case_key == default:
+        return case_lambda()
+    else:
+        return case_lambda(obj.value)
